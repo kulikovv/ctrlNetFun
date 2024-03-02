f4f3c7f068ed95c402038cb828a9c198e035337b
Kulikov Victor <kulikov.victor@gmail.com>
2024-03-03 01:59:14 +0400

    added setup py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from typing import List
+
+import setuptools
+
+
+def parse_requirements_txt(filename: str) -> List[str]:
+    with open(filename) as fd:
+        return list(filter(lambda line: bool(line.strip()), fd.read().splitlines()))
+
+
+setuptools.setup(
+    name="ctrlNetFun",  # Replace with your own username
+    version="0.0.1",
+    author="Victor Kulikov",
+    description="ControlNet package",
+    url="",
+    packages=['src'],
+    include_package_data=False,
+    python_requires=">=3.6, <4",
+    zip_safe=False,
+    entry_points={"console_scripts": [
+        "ctrlnet = src.inference:main",
+    ]},
+    install_requires=parse_requirements_txt("requirements.txt"),
+)
